30f3e3e45de47d6e5228234c7eb9275577713a71
evilnp300 <140300676+evilnp300@users.noreply.github.com>
2023-07-23 14:01:33 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showString("Hello!")
+input.setAccelerometerRange(AcceleratorRange.OneG)
+led.plot(0, 0)
+basic.forever(function () {
+	
+})
